3e635056c6643cc7c6649db54d017481f5bb6c55
Bimal kumar Mahato <54972204+biml107@users.noreply.github.com>
2024-01-07 23:05:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -119,5 +119,27 @@ function deleteMultipleTask() {
 
 
 
+const ObjectId = require('mongodb').ObjectId
+
+function validateMongoDbIds(ids)
+{
+    for(let id of ids){
+        if(!id)
+        {
+            return false;
+        }
+        if(!ObjectId.isValid(id))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+ 
+
+module.exports ={ validateMongoDbIds };
+
+
 
 
